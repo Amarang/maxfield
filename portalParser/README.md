07c9601f00f72171cf9d14457be1932f477bc2f3
Nick <typhoid.pwns@gmail.com>
2014-08-10 14:45:15 -0700

    add parser for intell map portals

---
+++ portalParser/README.md
@@ -0,0 +1,14 @@
+open Chrome
+hit F12
+go to ingress.com/intel
+drag/zoom the map to encompass all desired portals
+
+in the network tab of the developer tools window, search for getEntities. right click and "copy response" into a text editor. 
+
+first part of response should look like:
+{ "result": { "map": { "15_1502_3582_1_8_100": { "deletedGameEntityGuids": [ "636ce64052de47ecaae2da332793eee5.9",
+
+save the response
+
+then do
+python parser.py response.txt > output.csv
---
+++ portalParser/parser.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import json, sys
+
+def ascii(s):
+    return ''.join([c for c in s if ord(c) < 128])
+
+if len(sys.argv) < 2:
+    print "usage: python ingress.py [input json file]"
+    sys.exit()
+text = open(sys.argv[-1]).read()
+data = json.loads(text)
+
+
+for timestamps in data["result"]["map"].keys():
+    for portal in data["result"]["map"][timestamps]["gameEntities"]:
+        info = portal[2]
+        try:
+            temp = info["points"]
+            continue # ignore the fields
+        except:
+            pass
+
+        # ignore the links
+        if(info["type"] != "portal"): continue
+
+        # title, lat, lng = info["title"].encode('utf-8'), info["latE6"], info["lngE6"]
+        title, lat, lng = ascii(info["title"].strip()), info["latE6"], info["lngE6"]
+        print (title+",").ljust(55)+(str(lat)+",").ljust(10)+(str(lng)+",").ljust(10)+"  0, False"
